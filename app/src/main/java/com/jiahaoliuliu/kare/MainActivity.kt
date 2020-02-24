48497dc1c62992415d062d9dd438b462424075c9
Jiahao Liu <jiahaoliuliu@gmail.com>
2020-02-24 23:55:37 +0400

    Set a better version of maps

--- app/src/main/java/com/jiahaoliuliu/kare/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/kare/MainActivity.kt
@@ -3,15 +3,23 @@ package com.jiahaoliuliu.kare
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.jiahaoliuliu.kare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var googleMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
+        val supportMapFragment = supportFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
+        supportMapFragment.getMapAsync {
+            googleMap = it
+        }
     }
 }
