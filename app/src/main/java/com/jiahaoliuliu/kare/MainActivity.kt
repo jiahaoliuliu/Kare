29d6d27e370a9c276b6e882e64c77aa354824411
Jiahao Liu <jiahaoliuliu@gmail.com>
2020-02-28 14:00:22 +0400

    Improved the logic for the location. Set up the case when the location permission is denegated

--- app/src/main/java/com/jiahaoliuliu/kare/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/kare/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     private var googleMap: GoogleMap? = null
     private var locationPermissionGranted = false
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private var lastKnowLocation: Location? = null
+    private var lastKnownLocation: Location? = null
 
     companion object {
         private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1000
@@ -39,18 +39,14 @@ class MainActivity : AppCompatActivity() {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         supportMapFragment.getMapAsync {
             googleMap = it
-            // Turn on the my location layer and the related control on thee map
-            updateLocationUI()
-            // Get the current location of the device and set the position on thee map
-            getDeviceLocation();
+            getLocationPermission()
         }
     }
 
     private fun getLocationPermission() {
         if (ContextCompat.checkSelfPermission(
                 applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationPermissionGranted = true
-            updateLocationUI()
+            onLocationPermissionGuaranteed()
         } else {
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
                 PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
@@ -66,29 +62,41 @@ class MainActivity : AppCompatActivity() {
             PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
                 // If request is cancelled, the result arrays are empty
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    locationPermissionGranted = true
-                    updateLocationUI()
-                    getDeviceLocation()
+                    onLocationPermissionGuaranteed()
+                } else {
+                    googleMap?.let {googleMapNotNull ->
+                        lastKnownLocation?.let { lastKnownLocationNotNull ->
+                            googleMapNotNull.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocationNotNull.latitude, lastKnownLocationNotNull.longitude),
+                                DEFAULT_ZOOM))
+                        } ?: run {
+                            // if the permission is not guaranteed, then use the default location
+                            googleMapNotNull.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    DEFAULT_LOCATION,
+                                    DEFAULT_ZOOM
+                                )
+                            )
+                        }
+                        googleMapNotNull.uiSettings.isMyLocationButtonEnabled = false
+                    }
                 }
             }
         }
     }
 
+    private fun onLocationPermissionGuaranteed() {
+        locationPermissionGranted = true
+        // Turn on the my location layer and the related control on thee map
+        updateLocationUI()
+        // Get the current location of the device and set the position on thee map
+        getDeviceLocation();
+    }
+
     private fun updateLocationUI() {
         googleMap?.let {
-            try {
-                if (locationPermissionGranted) {
-                    it.isMyLocationEnabled = true
-                    it.uiSettings.isMyLocationButtonEnabled = true
-                } else {
-                    it.isMyLocationEnabled = false
-                    it.uiSettings.isMyLocationButtonEnabled = false
-                    // Set the last know location to null
-                    getLocationPermission()
-                }
-            } catch (exception: SecurityException) {
-                Log.e("Error","Unable to find user location", exception)
-            }
+            it.isMyLocationEnabled = true
+            it.uiSettings.isMyLocationButtonEnabled = true
         }
     }
 
@@ -104,9 +112,9 @@ class MainActivity : AppCompatActivity() {
             val locationResult = fusedLocationProviderClient.lastLocation
             locationResult.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    lastKnowLocation = task.result
+                    lastKnownLocation = task.result
                     it.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                        LatLng(lastKnowLocation!!.latitude, lastKnowLocation!!.longitude), DEFAULT_ZOOM)
+                        LatLng(lastKnownLocation!!.latitude, lastKnownLocation!!.longitude), DEFAULT_ZOOM)
                     )
                 } else {
                     Log.d(TAG, "Current location is null. Using defaults.");
