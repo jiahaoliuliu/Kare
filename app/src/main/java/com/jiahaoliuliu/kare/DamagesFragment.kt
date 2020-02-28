078fa5bd8d9d31929e9c832c0e6cf767bb2adfae
Jiahao Liu <jiahaoliuliu@gmail.com>
2020-02-29 00:13:36 +0400

    Using data binding for the damages fragment layout

--- app/src/main/java/com/jiahaoliuliu/kare/DamagesFragment.kt
+++ app/src/main/java/com/jiahaoliuliu/kare/DamagesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.jiahaoliuliu.kare.databinding.FragmentDamagesBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -12,15 +14,20 @@ import android.view.ViewGroup
  * create an instance of this fragment.
  */
 class DamagesFragment : Fragment() {
-    companion object {
-    }
+
+    private lateinit var binding: FragmentDamagesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_damages, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_damages, container, false)
+        return binding.root
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.front.
+    }
 }
